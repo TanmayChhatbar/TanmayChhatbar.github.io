bb4861741870a0a42fd766e4d21a71c40c5b9879
Tanmay <tanmaychhatbar@gmail.com>
2022-08-23 18:02:15 -0400

    rewrite speedy g info 2

--- python_generator/gen.py
+++ python_generator/gen.py
@@ -70,7 +70,7 @@ datas[-1].append([ttl, dt, lns, info])
 ttl = "Speedy G"
 dt = "\"I am Speed!\", said the Little Bird"
 lns = ["https://www.youtube.com/embed/T2X-0we-XOU"]
-info = "Who doesn't like fast vehicles? Named after the iconic Looney Tunes character, Speedy G is a tiny 600mm RC plane that we designed to use a 1300mAh 3S battery powering a 2207 T-Motor drone motor through a 35A ESC, spinning a 2-blade 7x4 prop.<br>We've measured doing over 160kph, around 100mph.<br>The design is almost entirely foamboard. The wings have no central spar. Instead, it is a stressed skin design. The front of the fuselage needed to be hollow for access to the battery, and is balsa. The motor mounts are 3D printed out of ABS."
+info = "Who doesn't like fast vehicles? Named after the iconic Looney Tunes character, Speedy G is a tiny 600mm RC plane that we designed to use a 1300mAh 3S battery powering a 2207 T-Motor drone motor through a 35A ESC, spinning a 2-blade 7x4 prop.<br><br>The design is almost entirely foamboard. The wings have no central spar. Instead, it is a stressed skin design. The front of the fuselage needed to be hollow for access to the battery, and is balsa. The motor mounts are 3D printed out of ABS.<br><br>We've measured doing over 160kph with GPS, around 100mph!"
 datas[-1].append([ttl, dt, lns, info])
 
 ttl = "The adventures of foomie"
