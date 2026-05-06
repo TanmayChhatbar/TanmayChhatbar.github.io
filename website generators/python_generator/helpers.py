c3c558a79c8688a51ec610e0bb98ef757071dd15
Tanmay Chhatbar <tanmaychhatbar@gmail.com>
2026-05-06 09:48:51 -0400

    update resume for award

--- website generators/python_generator/helpers.py
+++ website generators/python_generator/helpers.py
@@ -82,7 +82,7 @@ fullpage_div_start = """    <div id="fullPage">
                 </p>
                 
                 <div>
-                    <h4><a href="resume_0126.pdf" class="btn-custom">My Résumé</a> 🔗</h4>
+                    <h4><a href="resume_0526.pdf" class="btn-custom">My Résumé</a> 🔗</h4>
                     <br>
                     <h4><a href="#auto_model" class="btn-custom">My projects</a> 🔗</h4>
                     <br>
